4b48a5f61bee32c58d18f41e3c0bad26831d3753
Michal Kašpárek <michal.kasparek@gmail.com>
2023-04-21 12:26:35 +0200

    hlavní / vedlejší role done right

---
+++ src/gender.py
@@ -0,0 +1,27 @@
+def gender(jmeno):
+    if jmeno:
+        gender = ""
+        jmeno = jmeno.split("(")[0].strip()
+        if jmeno[-1:] == "á":
+            gender = "žena"
+        elif jmeno[-3:] == "ova":
+            gender = "žena"
+        elif "/ž/" in jmeno:
+            gender = "žena"
+        elif "Vica" in jmeno:
+            gender = "žena"
+        elif "Chantal" in jmeno:
+            gender = "žena"
+        elif "Lilian" in jmeno:
+            gender = "žena"
+        elif "Nataša" in jmeno:
+            gender = "žena"
+        elif "Beata" in jmeno:
+            gender = "žena"
+        elif "Suzanne" in jmeno:
+            gender = "žena"
+        else:
+            gender = "muž"
+    else:
+        gender = None
+    return gender
